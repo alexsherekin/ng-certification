6612382b9906d311a9de63a06f84fe92a11bfd2c
Oleksandr Sherekin <oleksandr.sherekin@deliveryhero.com>
2020-12-25 23:21:35 +0100

    Use real weather service

--- src/app/shared/services/weather/weather-service.interface.ts
+++ src/app/shared/services/weather/weather-service.interface.ts
@@ -2,11 +2,11 @@ import {Injectable} from '@angular/core';
 import {Observable} from 'rxjs';
 import {WeatherConditions} from '../../structures/weather-conditions';
 import {CoreModule} from '../../../core/core.module';
-import {WeatherFallbackService} from './weather-fallback.service';
+import {WeatherService} from './weather.service';
 
 @Injectable({
   providedIn: CoreModule,
-  useClass: WeatherFallbackService,
+  useClass: WeatherService,
 })
 export abstract class WeatherServiceInterface {
   abstract getByZip(zipCode: string): Observable<WeatherConditions>;
